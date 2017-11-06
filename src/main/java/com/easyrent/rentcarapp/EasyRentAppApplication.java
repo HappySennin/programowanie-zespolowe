9bd392e55bb74b149b47594989b81014acfd2ebb
HappySennin <229772@student.pwr.edu.pl>
2017-11-06 06:34:05 +0100

    Initial commit

---
+++ src/main/java/com/easyrent/rentcarapp/EasyRentAppApplication.java
@@ -0,0 +1,12 @@
+package com.easyrent.rentcarapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyRentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyRentAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/easyrent/rentcarapp/EasyRentAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.easyrent.rentcarapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EasyRentAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
