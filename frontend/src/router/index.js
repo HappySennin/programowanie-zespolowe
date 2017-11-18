df3b7d7e8539b26f65960c3e417dad72e3ff7033
dacjan <229787@student.pwr.edu.pl>
2017-11-18 01:43:15 +0100

    implement navigation to Reservations and Cars with vue router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,13 +1,15 @@
 import  Vue from 'vue'
 import Router from 'vue-router'
+import CarList from "../components/CarList/car-list.vue"
+import Reservation from "../components/Reservation/reservation.vue"
 
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/'
-    }
-  ]
+    { path: '/', component: CarList },
+    { path: '/reservation', component: Reservation }
+  ],
+  mode: "history"
 })
