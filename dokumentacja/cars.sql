b2682a8171fd453f8d574e2b153bb8c5a192094d
HappySennin <229772@student.pwr.edu.pl>
2017-12-04 05:19:09 +0100

    UMLs, sql init script

---
+++ dokumentacja/cars.sql
@@ -0,0 +1,15 @@
+insert into car (brand_name,model_name,power,price,type_name)
+values ('Mazda','Mazda 6','184 HP',450.0,'Sedan'),
+('Honda','Accord','189 HP',375.0,'Sedan'),
+('Audi','A8','333 HP',690.0,'Sedan'),
+('Chevrolet','SS','415 HP',750.40,'Sedan'),
+('Kia','Stinger','365 HP',620.55,'Sedan'),
+('Mercedes-Benz','S-class','463 HP',1099.99,'Sedan'),
+('Audi','Q7','252 HP',740.63,'SUV'),
+('Porsche','Cayenne','440 HP',1020.49,'SUV'),
+('Chevrolet','Traverse','310 HP',481.67,'SUV'),
+('Volvo','XC90','250 HP',364.13,'SUV'),
+('BMW','X1','228 HP',742.16,'SUV'),
+('Volkswagen','Crafter','140 HP',1605.36,'Truck'),
+('Fiat','Ducato L2H2','130 HP',1352.64,'Truck'),
+('Iveco','Daily','180 HP',1942.46,'Truck');
