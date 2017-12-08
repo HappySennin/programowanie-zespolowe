f432c34b69eabbe6c3424f2d183434153bb5bdfe
dacjan <229787@student.pwr.edu.pl>
2017-12-08 19:36:25 +0100

    let frontend receive data from sever (add  @CrossOrigin to Controller)

--- backend/src/main/java/com/easyrent/rentcarapp/controller/CarController.java
+++ backend/src/main/java/com/easyrent/rentcarapp/controller/CarController.java
@@ -14,51 +14,61 @@ public class CarController {
     @Autowired
     private CarService carService;
 
+    @CrossOrigin(origins = "http://localhost:8000")
     @RequestMapping(value = "/cars", method = RequestMethod.GET)
     public List<Car> getAllCars() {
         return carService.findAllCars();
     }
 
+    @CrossOrigin(origins = "http://localhost:8000")
     @RequestMapping(value = "/cars/id/{id}", method = RequestMethod.GET)
     public Car getCarById(@PathVariable Long id) {
         return carService.findCarById(id);
     }
 
+    @CrossOrigin(origins = "http://localhost:8000")
     @RequestMapping(value = "/cars/brand/{brand}", method = RequestMethod.GET)
     public List<Car> getCarsByBrand(@PathVariable String brand) {
         return carService.findByBrandName(brand);
     }
 
+    @CrossOrigin(origins = "http://localhost:8000")
     @RequestMapping(value = "/cars/model/{model}", method = RequestMethod.GET)
     public List<Car> getCarsByModel(@PathVariable String model) {
         return carService.findByModelName(model);
     }
 
+    @CrossOrigin(origins = "http://localhost:8000")
     @RequestMapping(value = "/cars/type/{type}", method = RequestMethod.GET)
     public List<Car> getCarsByType(@PathVariable String type) {
         return carService.findByTypeName(type);
     }
 
+    @CrossOrigin(origins = "http://localhost:8000")
     @RequestMapping(value = "/cars/power/{power}", method = RequestMethod.GET)
     public List<Car> getCarsByPower(@PathVariable String power) {
         return carService.findByPower(power);
     }
 
+    @CrossOrigin(origins = "http://localhost:8000")
     @RequestMapping(value = "/cars/price/{price}", method = RequestMethod.GET)
     public List<Car> getCarsByPrice(@PathVariable BigDecimal price) {
         return carService.findByPrice(price);
     }
 
+    @CrossOrigin(origins = "http://localhost:8000")
     @RequestMapping(value = "/cars", method = RequestMethod.PUT)
     public void updateCar(@RequestBody Car car) {
         carService.updateCar(car);
     }
 
+    @CrossOrigin(origins = "http://localhost:8000")
     @RequestMapping(value = "/cars", method = RequestMethod.POST)
     public void insertNewCar(@RequestBody Car car) {
         carService.saveCar(car);
     }
 
+    @CrossOrigin(origins = "http://localhost:8000")
     @RequestMapping(value = "/cars/{id}", method = RequestMethod.DELETE)
     public void deleteCarById(@RequestBody Long id) {
         carService.deleteCarById(id);
