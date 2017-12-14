c17573de1b198b94825922685dc298d5f49bab12
dacjan <229787@student.pwr.edu.pl>
2017-12-14 18:02:21 +0100

    generate Google API Key

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,14 @@ import ElementUI from 'element-ui';
 import locale from 'element-ui/lib/locale/lang/en'
 import VueResource from 'vue-resource'
 import store from './store/index'
+import * as VueGoogleMaps from 'vue2-google-maps'
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyAMzF0PCCzGm9K1j4qBK_Y1_IC93uNOB6Y',
+    libraries: 'places'
+  }
+})
 
 
 Vue.use(VueResource);
