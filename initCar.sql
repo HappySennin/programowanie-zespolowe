e6d18881cd5fe856c4c09f33390262eefbc6e3f0
TheNikram <wiormarcin@gmail.com>
2018-01-05 15:37:52 +0100

    Add files via upload

---
+++ initCar.sql
@@ -0,0 +1,21 @@
+INSERT INTO `car`(`id`, `brand_name`, `is_booked`, `latitude`, `longitude`, `model_name`, `power`, `price`, `type_name`, `url`) 
+VALUES 
+(1,'Mazda', 1 , 51.107583, 17.058409 , 'Mazda 6', '184 HP',450.0,'Sedan','http://cdn1.autoexpress.co.uk/sites/autoexpressuk/files/styles/article_main_image/public/1_mazda_front_cornering.jpg?itok=a52tpC1a'),
+(2,'Honda', 1 , 51.107771, 17.061177 , 'Accord' , '189 HP',375.0,'Sedan','https://media.ed.edmunds-media.com/honda/accord/2017/ot/2017_honda_accord_LIFE1_ot_1110162_1280.jpg'),
+(3,'Audi', 1 , 51.111008, 17.043882 , 'A8','333 HP',690.0,'Sedan','https://buyersguide.caranddriver.com/media/assets/submodel/7701.jpg'),
+(4,'Chevrolet', 0 , 51.111237, 17.038249 , 'SS','415 HP',750.40,'Sedan', 'http://www.chevrolet.com/content/dam/chevrolet/na/us/english/index/vehicles/2017/performance/ss-sedan/mov/01-images/2017-ss-performance-01.jpg?imwidth=1200'),
+(5,'Kia', 0 , 51.110410, 16.883634 ,'Stinger','365 HP',620.55,'Sedan','http://bi.gazeta.pl/im/5c/3c/14/z21217628IE,Kia-Stinger.jpg'),
+(6,'Mercedes-Benz', 1 , 51.110019, 16.882282 , 'S-class','463 HP',1099.99,'Sedan', 'https://s.aolcdn.com/dims-global/dims3/GLOB/legacy_thumbnail/788x525/quality/85/https://s.aolcdn.com/commerce/autodata/images/USC40MBC783A021001.jpg'),
+(7,'Audi' , 1 , 51.110772, 16.877307 , 'Q7','252 HP',740.63,'SUV','https://hips.hearstapps.com/amv-prod-cad-assets.s3.amazonaws.com/images/10q2/339155/2011-audi-q7-photo-341327-s-original.jpg'),
+(8,'Porsche' , 1 , 51.110120, 16.878967 , 'Cayenne','440 HP',1020.49,'SUV','https://s.aolcdn.com/commerce/autodata/images/USC60PRS011A021001_2.jpg'),
+(9,'Chevrolet', 0 , 51.106452, 17.078616 , 'Traverse','310 HP',481.67,'SUV','https://media.ed.edmunds-media.com/chevrolet/traverse/2017/ot/2017_chevrolet_traverse_LIFE1_ot_207172_1280.jpg'),
+(10,'Volvo', 0 , 51.113494, 16.994534 , 'XC90','250 HP',364.13,'SUV','https://assets.volvocars.com/pl/~/media/shared-assets/images/galleries/new-cars/new-xc90/landing/story-grid/grid2_vcc08428.jpg?w=376'),
+(11,'BMW' , 1 , 51.099439, 17.040144 , 'X1','228 HP',742.16,'SUV','https://media.ed.edmunds-media.com/bmw/x1/2017/oem/2017_bmw_x1_4dr-suv_xdrive28i_rq_oem_3_1280.jpg'),
+(12,'Volkswagen', 1 , 51.108065, 16.954443 , 'Crafter','140 HP',1605.36,'Truck','https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/VW_Crafter_2.0_TDI_Facelift.JPG/1200px-VW_Crafter_2.0_TDI_Facelift.JPG'),
+(13,'Fiat', 1 , 51.107822, 17.034419 , 'Ducato L2H2','130 HP',1352.64,'Truck','https://otomotopl-imagestmp.akamaized.net/images_otomotopl/851417721_2_1080x720_metalik-dodaj-zdjecia.jpg'),
+(14,'Iveco', 0 , 51.126718, 17.045269 , 'Daily','180 HP',1942.46,'Truck','https://www.iveco.com/Common/PublishingImages/iveco-daily-e6/van/promo/new-daily-promotion.jpg'),
+(15,'Audi', 0 , 51.125032, 16.988492 , 'R8 Coupe Trims','540 HP',2500.00,'Sport','https://s.aolcdn.com/commerce/autodata/images/CAC70AUC171A021001.jpg'),
+(16,'Mercedes-Benz', 1 , 51.157033, 16.978610 , 'Maybach','463 HP',1300.00,'Luxury','https://www.mercedes-benz.com/wp-content/uploads/sites/3/2017/08/00-mercedes-benz-vehicles-vision-mercedes-maybach-6-cabriolet-3400x1440.jpg');
+
+select * from car
+ 
---
+++ initUser.sql
@@ -0,0 +1,10 @@
+INSERT INTO `app_user` (`id`, `is_super_user`, `login`, `password`) VALUES
+(1, b'1', 'admin', '$2a$10$mSKPu2OOTfOuco37akashOG/twMv7r8nzc3hkgr/ZFU7jnDXRsE5G'),
+(2, b'0', 'user', '$2a$10$tGPQ1LKZwknAhjj.g9k9C.bW5nXrae9VYKnT5Pql.JCNLtr9Ga9ii'),
+(3, b'0', 'driver', '$2a$10$RyTIh4B1gBbWVT1CsDywV.Wp07W44Cfl8Icj//ya4bBYmL.7O/3NO'),
+(4, b'0', 'tester', '$2a$10$jwtuSk9E2D525z.tr76TKeDG5nmihhH6AujpNRj5RL2UT1J3t8BFi'),
+(5, b'0', 'bot', '$2a$10$WEPM1mq2ypKLMY5uEmJ3feMaTE13l.vU1cBWA9HaQTE44nBmHd8AO'),
+(6, b'0', 'renter', '$2a$10$3wZ76uxzJWU2jveAy.QIH.gjPgh92Tfn83bc9Wi6hh4c2rG6E3W8y'),
+(7, b'0', 'blaster', '$2a$10$rbuWBhZn887N4PnsA4uR1.kEMW0q9KzkdNPOVUGsVo5qWiJZYXuP6');
+select * from app_user
+
